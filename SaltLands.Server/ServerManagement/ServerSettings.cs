d022d0ea51779f2582d4141200c74b8dd4223e50
Kyle Kemp <kyle@seiyria.com>
2024-06-01 10:01:33 -0500

    move some values to a different config

--- SaltLands.Server/ServerManagement/ServerSettings.cs
+++ SaltLands.Server/ServerManagement/ServerSettings.cs
@@ -25,23 +25,8 @@ public class ServerSettings
     // the default timeout (in ms) for players to get disconnected after if they don't send anything to the game
     int ClientConnectionTimeout = 150;
 
-    // how many chunks away to keep alive for nearby players.
-    int MaxChunkRadius = 12;
-
     // the maximum number of blocks that can tick per server tick
     int MaxMainThreadBlockTicks = 10000;
-
-    // how many random blocks should tick per chunk. higher values tick more blocks per chunk. more random ticks = more world processing, like grass growing, etc.
-    int RandomBlockTicksPerChunk = 16;
-
-    // number of ms between server ticks. we want 30 ticks per second.
-    float TickTime = 1000 / 30;
-
-    // how far away from the player in terms of chunks can get random ticks.
-    int BlockTickChunkRange = 4;
-
-    // how long (in ms) each random tick should be spaced apart. lower numbers mean faster ticking.
-    int BlockTickInterval = 300;
 
     /* Chat values */
 
--- SaltLands.World/WorldGeneration/WorldSettings.cs
+++ SaltLands.World/WorldGeneration/WorldSettings.cs
@@ -5,6 +5,12 @@ namespace SaltLands;
 
 public class WorldMagicNumbers
 {
+    // number of ms between server ticks. we want 30 ticks per second.
+    float TickTime = 1000 / 30;
+
+    // how many ms between each save of the world
+    int AutoSaveInterval = 300 * 1000;
+
     // how many chunks away an entity must be within to get updates
     int DefaultEntityTrackingRange = 4;
 
@@ -16,6 +22,9 @@ public class WorldMagicNumbers
 
     // size of the chunk ready buffer. if full, will not generate new chunks until it's less full.
     int ReadyChunkQueueSize = 200;
+
+    // how many chunks away to keep alive for nearby players.
+    int ChunkRadiusMax = 6;
 
     // number of chunks the system may send per tick.
     int ChunksToSendPerTick = 32;
@@ -32,14 +41,20 @@ public class WorldMagicNumbers
     // how often the chunk generator should run (in ms)
     int ChunkThreadTickTime = 10;
 
-    // how many ms between each save of the world
-    int AutoSaveInterval = 300 * 1000;
-
     // width and length of the number of chunks to load for the spawn area
     int SpawnChunksWidth = 8;
 
     // how many entities can naturally spawn in the world per game tick
     int MaxEntitySpawnsPerTick = 8;
+
+    // how many random blocks should tick per chunk. higher values tick more blocks per chunk. more random ticks = more world processing, like grass growing, etc.
+    int RandomBlockTicksPerChunk = 16;
+
+    // how far away from the player in terms of chunks can get random ticks.
+    int BlockTickChunkRange = 4;
+
+    // how long (in ms) each random tick should be spaced apart. lower numbers mean faster ticking.
+    int BlockTickInterval = 300;
 }
 
 public class WorldSettings
