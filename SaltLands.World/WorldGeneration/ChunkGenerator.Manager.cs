79aa3bf48ab0fc4e01ca639f0452692bcb311d94
Kyle Kemp <kyle@seiyria.com>
2024-06-06 14:00:56 -0500

    add files

---
+++ SaltLands.World/WorldGeneration/ChunkGenerator.Manager.cs
@@ -0,0 +1,74 @@
+﻿using System.Numerics;
+
+namespace SaltLands.WorldGenerator;
+
+internal partial class ChunkGenerator
+{
+    #region Public Fields
+
+    public Dictionary<string, Chunk> ChunkMap = [];
+
+    #endregion Public Fields
+
+    #region Public Properties
+
+    public List<Chunk> LoadedChunks { get; private set; } = [];
+
+    #endregion Public Properties
+
+    #region Public Methods
+
+    public Chunk? GetChunk(int chunkX, int chunkY)
+    {
+        var id = ChunkId(chunkX, chunkY);
+        if (!ChunkMap.ContainsKey(id)) return null;
+
+        var chunk = ChunkMap[id];
+
+        return chunk;
+    }
+
+    public Chunk LoadChunk(int chunkX, int chunkY, bool requestedFromAnotherChunk = false)
+    {
+        var id = ChunkId(chunkX, chunkY);
+        var chunk = GetChunk(chunkX, chunkY);
+        if (chunk != null) return chunk;
+
+        chunk = new Chunk()
+        {
+            LastLoadedAt = DateTime.Now.Ticks,
+            Position = new Vector2(chunkX, chunkY)
+        };
+
+        GenerateChunk(chunk, requestedFromAnotherChunk);
+
+        if (!requestedFromAnotherChunk)
+        {
+            LoadedChunks.Add(chunk);
+            ChunkMap.Add(ChunkId(chunkX, chunkY), chunk);
+        }
+
+        return chunk;
+    }
+
+    public void UnloadChunk(int chunkX, int chunkY)
+    {
+        var id = ChunkId(chunkX, chunkY);
+        var chunk = GetChunk(chunkX, chunkY);
+        if (chunk == null) return;
+
+        LoadedChunks.Remove(chunk);
+        ChunkMap.Remove(id);
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    private static string ChunkId(int chunkX, int chunkY)
+    {
+        return $"{chunkX},{chunkY}";
+    }
+
+    #endregion Private Methods
+}
---
+++ SaltLands.World/WorldGeneration/ChunkGenerator.Position.cs
@@ -0,0 +1,117 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SaltLands.WorldGenerator;
+
+internal class Centroid
+{
+    #region Public Fields
+
+    public float Height;
+    public float Moisture;
+    public Vector2 Position;
+    public float Temperature;
+
+    #endregion Public Fields
+}
+
+internal class VoronoiRenderData
+{
+    #region Public Fields
+
+    public List<Centroid> Centroids;
+    public List<int> HalfEdges;
+    public int NumEdges;
+    public int NumRegions;
+    public int NumTriangles;
+    public List<Vector2> Points;
+    public List<int> Triangles;
+
+    #endregion Public Fields
+}
+
+internal partial class ChunkGenerator
+{
+    #region Private Delegates
+
+    private delegate void ChunkTileIterator(Chunk chunk, Tile tile);
+
+    #endregion Private Delegates
+
+    #region Private Methods
+
+    private static float AverageInRadius(FastNoiseLite generator, int cellX, int cellY, int radius = 1)
+    {
+        float totalValue = 0;
+
+        for (int x = cellX - radius; x < cellX + radius; x++)
+        {
+            for (int y = cellY - radius; y < cellY + radius; y++)
+            {
+                totalValue += generator.GetNoise(x, y);
+            }
+        }
+
+        return totalValue / (radius * radius);
+    }
+
+    private static List<Tile> ChunkTilesToList(Chunk chunk)
+    {
+        List<Tile> tiles = [];
+
+        for (int x = 0; x < chunk.Tiles.Length; x++)
+        {
+            for (int y = 0; y < chunk.Tiles[x].Length; y++)
+            {
+                tiles.Add(chunk.Tiles[x][y]);
+            }
+        }
+
+        return tiles;
+    }
+
+    private static double DistanceBetween(Vector2 start, Vector2 end)
+    {
+        double dx = start.X - end.X;
+        double dy = start.Y - end.Y;
+
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+
+    private static Centroid GetClosestCentroid(List<Centroid> points, Vector2 tilePosition)
+    {
+        return points.OrderBy(c => DistanceBetween(c.Position, tilePosition)).First();
+    }
+
+    private static float NormalizedNoise(FastNoiseLite generator, float x, float y)
+    {
+        float wavelength = 0.5f;
+        return 1 + generator.GetNoise(x / wavelength, y / wavelength) / 2;
+    }
+
+    // turn a string seed into a numeric one
+    private static int NumericSeed(string seed)
+    {
+        return BitConverter.ToInt32(MD5.HashData(Encoding.UTF8.GetBytes(seed)));
+    }
+
+    private Vector2 RealPosition(Chunk chunk, Tile tile)
+    {
+        return new Vector2(RealX(chunk, tile), RealY(chunk, tile));
+    }
+
+    private int RealX(Chunk chunk, Tile tile)
+    {
+        int chunkSize = Settings.MagicNumbers.ChunkSize;
+        return (int)((chunk.Position.X * chunkSize) + tile.Position.X);
+    }
+
+    private int RealY(Chunk chunk, Tile tile)
+    {
+        int chunkSize = Settings.MagicNumbers.ChunkSize;
+        return (int)((chunk.Position.Y * chunkSize) + tile.Position.Y);
+    }
+
+    #endregion Private Methods
+}
